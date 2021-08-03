b34d3b102d9064c2d2b8e3bba1f268584eeabea3
brandonO91 <brandon.oyama.0526@gmail.com>
2021-08-03 15:41:43 -0700

    adding form and small logi

--- myapp/app.py
+++ myapp/app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'mysecret'
 
+class LogInForm(FlaskForm):
+    userName = StringField("User Name")
+    password = PasswordField("Password")
+    submit = SubmitField('Submit User')
+
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', template_form=LogInForm())
