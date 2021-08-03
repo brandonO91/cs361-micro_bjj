7df2025cbd72eccf8d7fd7e31ad54d7665898515
brandonO91 <brandon.oyama.0526@gmail.com>
2021-08-03 14:37:18 -0700

    adding code to app.py

---
+++ myapp/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecret'
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    return "hello world"
