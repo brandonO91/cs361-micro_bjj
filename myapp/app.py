2867c0971f9a3d5ab2d82e607d0390e064521cad
brandonO91 <brandon.oyama.0526@gmail.com>
2021-08-03 21:51:44 -0700

    added new user route

--- myapp/app.py
+++ myapp/app.py
@@ -12,10 +12,17 @@ class LogInForm(FlaskForm):
     password = PasswordField("Password")
     submit = SubmitField('Submit User')
 
+# login page - use microservice
 @app.route('/', methods=["GET", "POST"])
 def index():
     return render_template('index.html', template_form=LogInForm())
 
+# if there is no user in DB
+@app.route('/newUser', methods=["GET","POST"])
+def createJuji():
+    return 'Creating new jiujitiero'
+
+# this will be first page a user goes to after logging in
 @app.route('/technique/<int:user_id>', methods=["GET", "POST"])
 def technique(user_id):
     return f'User {user_id} techniques'
